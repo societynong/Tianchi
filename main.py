dd94c6e596b4638f3adfb7000d2938f920cf8fe5
societynong <172065208@qq.com>
2018-11-23 09:14:44 +0800

    The orignal version

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from sklearn.linear_model import  LogisticRegression
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
 
-df = pd.read_csv('zhengqi_train.txt')
+df = pd.read_table('zhengqi_train.txt')
 
-df.head(10)
+data_features = df.drop('target',axis=1)
+data_target = df.target
+print(data_target.head(10))
